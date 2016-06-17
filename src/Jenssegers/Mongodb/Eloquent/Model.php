b7543dfc3dc0d31f236fff7738f8dc77876d7eec
David Soto <dsoto@tpipr.com>
2016-06-17 17:12:40 -0400

    Add auto casting to models.

--- src/Jenssegers/Mongodb/Eloquent/Model.php
+++ src/Jenssegers/Mongodb/Eloquent/Model.php
@@ -554,4 +554,45 @@ abstract class Model extends BaseModel
 
         return parent::__call($method, $parameters);
     }
+
+/**
+ * Create a new instance of the given model.
+ *
+ * @param array $attributes
+ * @param bool  $exists
+ *
+ * @return static
+ */
+public function newInstance($attributes = [], $exists = false)
+{
+    // This method just provides a convenient way for us to generate fresh model
+    // instances of this current model. It is particularly useful during the
+    // hydration of new objects via the Eloquent query builder instances.
+
+    $class = $attributes['className'] ?? static::class;
+    $model = new $class((array) $attributes);
+
+    $model->exists = $exists;
+
+    return $model;
+}
+
+/**
+ * Create a new model instance that is existing.
+ *
+ * @param array       $attributes
+ * @param string|null $connection
+ *
+ * @return static
+ */
+public function newFromBuilder($attributes = [], $connection = null)
+{
+    $model = $this->newInstance($attributes, true);
+
+    $model->setRawAttributes((array) $attributes, true);
+
+    $model->setConnection($connection ?: $this->connection);
+
+    return $model;
+}
 }
