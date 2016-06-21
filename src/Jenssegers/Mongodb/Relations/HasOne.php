050fa1c542baec4322fe91ce8d01d836dcf202ba
David Soto <dsoto@tpipr.com>
2016-06-21 17:01:18 -0400

    Added new method to HasOne

--- src/Jenssegers/Mongodb/Relations/HasOne.php
+++ src/Jenssegers/Mongodb/Relations/HasOne.php
@@ -49,4 +49,33 @@ class HasOne extends EloquentHasOne
     {
         return $this->getForeignKey();
     }
+
+    /**
+     * Build model dictionary keyed by the relation's foreign key.
+     *
+     * @param \Illuminate\Database\Eloquent\Collection $results
+     *
+     * @return array
+     */
+    protected function buildDictionary(\Illuminate\Database\Eloquent\Collection  $results)
+    {
+        $dictionary = [];
+
+        $foreign = $this->getPlainForeignKey();
+
+        // First we will create a dictionary of models keyed by the foreign key of the
+        // relationship as this will allow us to quickly access all of the related
+        // models without having to do nested looping which will be quite slow.
+        foreach ($results as $result) {
+            if (is_array($result->{$foreign})) {
+                array_map(function ($r) use (&$dictionary, $result) {
+                    $dictionary[$r][] = $result;
+                }, $result->{$foreign});
+            } else {
+                $dictionary[$result->{$foreign}][] = $result;
+            }
+        }
+
+        return $dictionary;
+    }
 }
